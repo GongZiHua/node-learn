2fba5c42cd096a39a941b7084da78d11aa504d62
qianxun <945673200@qq.com>
2017-10-26 23:47:43 +0800

    http

--- start.js/first.js
+++ start.js/first.js
@@ -1,1 +1,13 @@
-console.log("learn node")
+
+var http = require('http');
+http.createServer(function(req, res){
+	console.log(req.method);
+	console.log(req.url);
+	console.log(req.headers);
+	res.statusCode = 404;
+	res.setHeader('Content-Type','text/html;charset=utf-8');
+	res.setHeader('name','dsada');
+	res.write(new Date().toString());
+	res.end();
+}).listen(8080,'localhost');
+console.log('Server is runnig at 127.0.0.1')
